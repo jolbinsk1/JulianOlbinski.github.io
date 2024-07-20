d04e5cf9434a26293bc976abe6b16a310dfda298
Julian Olbinski <163324229+jolbinsk1@users.noreply.github.com>
2024-07-20 15:44:37 +0200

    Delete html5up-dimension directory

--- html5up-dimension/background_fade.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var bg = document.getElementById('bg');
-    bg.classList.add('visible'); // Add class to show background with fade-in transition
-});
